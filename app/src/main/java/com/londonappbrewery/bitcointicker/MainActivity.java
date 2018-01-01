30fbabd91fd8dfcf969a7168f4573184a7b8baef
lnbatioco <LoryleenBatioco@gmail.com>
2017-12-31 19:43:44 -0800

    add bitcoinaverage base url and spinner listener

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         mPriceTextView = (TextView) findViewById(R.id.priceLabel);
         Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
 
+        // Starter code for spinner provided by: developer.android.com
         // Create an ArrayAdapter using the String array and a spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.currency_array, R.layout.spinner_item);
@@ -44,7 +45,20 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+        // TODO: Set an OnItemSelected listener on the spinner (item is the currency)
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitcoin", "" + parent.getItemAtPosition(position)); // will get value of index: currency name
+                Log.d("Bitcoin", "Position is: " + position); // ex) position for CAD is 2, it is the index of an array
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // if nothing is selected!
+                Log.d("Bitcoin", "Nothing selected");
+            }
+        });
 
     }
 
